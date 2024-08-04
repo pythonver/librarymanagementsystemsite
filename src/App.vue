1a9dcfc9e6542d15700c9f3dd802a12b03e4bf2e
ascion <pubgld@foxmail.com>
2024-08-05 06:52:58 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="background">
+    <BookTable></BookTable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BookTable from "./components/BookTable.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BookTable
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+
+}
+.background {
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
 }
 </style>
---
+++ src/components/BookTable.vue
@@ -0,0 +1,63 @@
+<template>
+    <table class="books">
+        <tr>
+          <th>图书ID</th>
+          <th>书名</th>
+          <th>作者</th>
+          <th>出版</th>
+          <th>出版社</th>
+          <th>页数</th>
+        </tr>
+        <tr v-for="item in booksList " :key="item.id" id="mainConnent">
+            <th>{{ item.id }}</th>
+            <th>{{ item.title }}</th>
+            <th> {{ item.author }}</th>
+            <th>{{ item.publish }}</th>
+            <th>{{ item.publisher }}</th>
+            <th>{{ item.theNumberOfPage }}</th>
+        </tr>
+    </table>
+    
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+    name: "BookTable",
+    data(){
+         return {
+            booksList:[]
+         }
+    },
+    mounted(){
+        axios({
+            url:"http://localhost:8443/library",
+            params:{
+                "getAll": "yes"
+            }
+        }).then(res=>{
+            console.log(res);
+            this.booksList = res.data
+
+            
+        }).catch(err=>{
+            console.log(err);
+
+            
+        })
+
+
+    },
+    method:{
+        
+    }
+}
+</script>
+
+<style scoped>
+#mainConnent th{
+    border: 1px solid black;
+
+}
+</style>
